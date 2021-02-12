78f1d401282d85c3a1fd9d18665a0e0fb5d2f988
Arindam-Jain <arindamjain18@gmail.com>
2021-02-12 22:29:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# DEV267 - Automated SAP Fiori Apps Testing with​ Continuous Delivery
+
+[![REUSE status](https://api.reuse.software/badge/github.com/SAP-samples/teched2020-DEV267)](https://api.reuse.software/info/github.com/SAP-samples/teched2020-DEV267)
+
+## Description
+
+This repository contains the material for the SAP TechEd 2020 session called DEV267 - Automated SAP Fiori Apps Testing with​ Continuous Delivery
+
+## Overview
+SAPUI5, as a UI technology, provides all the resources you need to build SAP Fiori apps. The quality of any app is best guaranteed by a set of automated tests. A good toolset is crucial to simplify the process, save time, and increase the development efficiency. In this hands-on session, you learn how to setup your DevOps tools chain, create tests for SAP Fiori apps, and run them in your continuous delivery pipeline.
+
+## Presentation
+You can find the presentation [here](DEV267.pdf)
+
+## Exercise
+You can access the exercise guide in the [Tutorial Navigator](https://developers.sap.com/tutorials/cp-uiveri5-cap.html)
+
+## Troubleshooting
+in case of issues please also check [Troubleshooting guide](troubleshooting.md)
+## How to obtain support
+
+Support for the content in this repository is available during the actual time of the online session for which this content has been designed. Otherwise, you may request support via the [Issues](../../issues) tab.
+
+## License
+Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, version 2.0 except as noted otherwise in the [LICENSE](LICENSES/Apache-2.0.txt) file.
---
+++ fiori/app/admin/webapp/test/uiveri5/pages/newBookPage.js
@@ -0,0 +1,92 @@
+module.exports = createPageObjects({
+  NewBook: {
+    // replace <iDoAction> under actions with the method defined in teched.spec.js
+    actions: {
+      iEnterTitle: function (sTitle) {
+          element(by.control({
+              id: "admin::BooksDetailsList--fe::EditableHeaderForm::EditableHeaderTitle::Field-inner",
+              interaction: {
+                  idSuffix: "inner"
+              },
+          })
+        ).sendKeys(sTitle);
+      },
+
+      iSelectGenre: function () {
+          element(by.control({
+              id: "admin::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::genre_ID::Field-inner",
+          })).click();
+
+          element(by.control({
+              controlType: "sap.ui.mdc.Field",
+              viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+              bindingPath: {
+                  path: "/Genres(10)",
+                  propertyPath: "ID"
+              }
+          })).click();
+
+          element(by.control({
+              id: "admin::BooksDetailsList--fe::FormContainer::FieldGroup::General::FieldValueHelp::genre_ID-ok",
+              interaction: {
+                  idSuffix: "BDI-content"
+              }
+          })).click();
+        },    
+      iSelectAuthor: function () {
+          element(by.control({
+              id: "admin::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::author_ID::Field-inner-vhi",
+            })
+          ).click();
+
+          element(by.control({
+              controlType: "sap.m.Text",
+              viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+              properties: {
+                  text: "107",
+              },
+            })
+          ).click();
+
+          element(by.control({
+              id: "admin::BooksDetailsList--fe::FormContainer::FieldGroup::General::FieldValueHelp::author_ID-ok",
+              interaction: {
+                  idSuffix: "BDI-content"
+              },
+          })).click();
+      },
+
+      iClickOnSaveButton: function () {
+          element(by.control({
+              id: "admin::BooksDetailsList--fe::FooterBar::StandardAction::Save",
+          })
+        ).click();
+      },
+
+      iNavigateBack: function () {
+        element(by.control({
+            id: "backBtn",
+        })
+      ).click();
+    },  
+      
+
+    },
+
+    assertions: {
+      
+        
+
+      iSeeEditButton: function () {
+          var editButton =element(by.control({
+              id: "admin::BooksDetailsList--fe::StandardAction::Edit",
+              interaction: {
+                  idSuffix: "content",
+              },
+          })
+        );
+        expect(editButton.isPresent()).toBeTruthy();
+      },
+    },
+  },
+});
